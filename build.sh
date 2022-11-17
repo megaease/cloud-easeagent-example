092b191c7f03555209df023fa8cbcaad26f5f1e9
yufu.deng <observeralone@gmail.com>
2022-11-17 22:26:16 +0800

    feat: add build.sh

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+pushd `dirname $0` > /dev/null
+SCRIPT_PATH=`pwd -P`
+cd $SCRIPT_PATH/
+
+mkdir -p $SCRIPT_PATH/bin/java
+cd java-user-list
+mvn clean install -Dmaven.test.skip && cp target/java-user-list-1.0.0.jar $SCRIPT_PATH/bin/java/ && cd $SCRIPT_PATH
+
+mkdir -p $SCRIPT_PATH/bin/go
+cd go-admin
+go build && mv go-admin $SCRIPT_PATH/bin/go/ && cd $SCRIPT_PATH
