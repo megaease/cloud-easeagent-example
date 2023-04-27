0b446c374064daaa5f3c6594397f0f4a7db2fcf6
observeralone <observeralone@gmail.com>
2023-04-27 22:09:37 +0800

    use easeagent 0.0.3

--- net-manager/resources/scripts/base_image_build.sh
+++ net-manager/resources/scripts/base_image_build.sh
@@ -3,4 +3,4 @@ pushd `dirname $0` > /dev/null
 SCRIPT_PATH=`pwd -P`
 cd $SCRIPT_PATH/
 
-docker build -f ./Dockerfile . -t megaease/net-apm-manager-example:1.0.0
+docker build -f ./Dockerfile . -t megaease/net-apm-manager-example:1.0.1
